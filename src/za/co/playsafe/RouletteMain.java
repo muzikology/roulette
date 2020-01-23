c4d662eec0014743f07ef577b3112986d90c7a87
Muzi <muzikhubeka@gmail.com>
2020-01-23 18:19:54 +0200

    playsafe project submission

--- src/za/co/playsafe/RouletteMain.java
+++ src/za/co/playsafe/RouletteMain.java
@@ -1,8 +1,12 @@
 package za.co.playsafe;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
 
 
 public class RouletteMain {
@@ -21,16 +25,43 @@ public class RouletteMain {
 	       int result;
 	       char response = 'y';
 	       int resultArr[] = new int[36];
+	       ArrayList<String> playerNames = new ArrayList<String>();
+	       
+	       
+	       try {
+	    	   /* Load list of player names from file**/
+	    	   File file = new File("C:\\dev\\playsafe_app\\roulette\\playerNames.txt"); 
+	    	   
+	    	   BufferedReader br = new BufferedReader(new FileReader(file)); 
+	    	   
+	    	   String st; 
+
+				while ((st = br.readLine()) != null) { 
+					playerNames.add(st);
+				     System.out.println(st);
+				   }
+			} catch (IOException e) {
+				e.printStackTrace();
+			} 
 
 	       while (response == 'y' || response == 'Y' && total <= 0)
 	       {
-	           System.out.print("Enter your bet amount: ");
+	    	   String name = null;
+	    	   System.out.print("Enter your Player Name: ");
+	    	   String p1 = keyboard.nextLine();
+	    	   for(String str: playerNames) {
+	    		   if(str.equals(p1)) {
+	    			   name = str;
+	    		   }
+	    	   }
+	    	   System.out.println("PlayerName is :" + name);
+	           System.out.print("Enter your bet amount " + name + ": ");
 	           amount = keyboard.nextDouble();
 	           System.out.print("0 - Even\n1 - Odd\n2 - Number\n");
 	           choice = -1;
 	           while (choice < 0 || choice > 2)
 	           {
-	               System.out.print("Place your bet on: ");
+	               System.out.print("Place bet on: ");
 	               choice = keyboard.nextInt();
 	           }
 	           number = 0;
@@ -38,7 +69,7 @@ public class RouletteMain {
 	           {
 	               while (number < 1 || number > 36)
 	               {
-	                   System.out.print("Place your bet on number(1-36): ");
+	                   System.out.print("Place bet on number(1-36): ");
 	                   number = keyboard.nextInt();
 	               }
 	           }
@@ -64,7 +95,7 @@ public class RouletteMain {
 	           //Print out game result, win/lose amount
 	           if (result > 0)
 	           {
-	               System.out.println("Congratulatons!!! You win!");
+	               System.out.println("Hi " + name + "Congratulatons You win!");
 	               System.out.printf("You have won R%.2f \n", result * amount);
 	               System.out.printf("Here's your money back: R%.2f \n",
 	                       (result + 1) * amount);
@@ -75,7 +106,7 @@ public class RouletteMain {
 	           }
 	           else
 	           {
-	               System.out.println("You lose. Better luck next time!");
+	               System.out.println("Hi " + name + "You lose");
 	               System.out.printf("You have lost R%.2f \n",
 	                       (result + 1) * amount);
 	               total = total - (result + 1) * (amount);
@@ -96,11 +127,11 @@ public class RouletteMain {
 	        	}
 	           
 	           
-	           System.out.println("You hayve R" + total + " remaining." );
+	           System.out.println(name + "You have R" + total + " remaining." );
 	           System.out.println("You have won " + win + " games.");
 	           System.out.println("You have lost " + lose + " games.");
 	           System.out.println("The wheel has been spun " + spin + " times.");          
-	           System.out.print("\nWould you like to play another game? (y/n) ");
+	           System.out.print("\nWould you like to play another game? (y/n) " + name);
 	           response = keyboard.next().charAt(0);
 	           
 	           
